7f450aefa71e58e5121364416f62ff1726d4ef6a
Nolit <yaboonjapan@gmail.com>
2017-01-15 22:46:53 +0900

    マイスタンプ帳ページ レスポンスデータに専用のクラスを使用

--- src/main/java/servlets/MyStampBookServlet.java
+++ src/main/java/servlets/MyStampBookServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.StdTypeResolverBuilder;
+import data.StampData;
 import database.entities.Stamps;
 import database.managers.StampManager;
 import database.managers.UserManager;
@@ -40,31 +41,31 @@ public class MyStampBookServlet extends HttpServlet {
         referenceUserName = um.read(referenceUserId).getUserName();
         System.out.println("デバッグ:referenceUserName"+um.read(referenceUserId).getUserName());
         
-        Map<String, Object> myStampBook = copy(sm.getMyStampBook(referenceUserId));
+        StampData[] stampDataSet = copy(sm.getMyStampBook(referenceUserId));
         
         //読み込み
         ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writeValueAsString(myStampBook);
+        String json = mapper.writeValueAsString(stampDataSet);
         try (PrintWriter out = response.getWriter()) {
+            out.println(referenceUserName);
             out.println(json);
         }
     }
     
-    private Map<String, Object> copy(List<Stamps> fromObj){
-        Map<String, Object> retStampMap = new HashMap<>();
-        ArrayList<Map<String, Object>> stamps = new ArrayList<>();
-        retStampMap.put("userName", referenceUserName);
-        for(Stamps i:fromObj){
-            Map<String, Object> stamp = new HashMap<>();
-            stamp.put("stampId", i.getStampId());
-            stamp.put("picture", Base64.encodeBase64(ImageUtil.read(i.getPicturePass())));
-            stamp.put("stampName", i.getStampName());
-            stamp.put("stampDate", i.getStampDate().toString());
-            stamp.put("stampRallyName", i.getStampRallysList().get(0).getStamprallyName());
-            stamps.add(stamp);
+    private StampData[] copy(List<Stamps> fromObj){
+        StampData[] stampDataSet = new StampData[fromObj.size()];
+        for(int index = 0;index < fromObj.size();index++){
+            Stamps i = fromObj.get(index);
+            StampData data = new StampData();
+            data.setStampId(i.getStampId());
+            data.setPicture(ImageUtil.read(i.getPicturePass()));
+            data.setStampName(i.getStampName());
+            data.setStampDate(i.getStampDate().toString());
+            data.setStampRallyName(i.getStampRallysList().get(0).getStamprallyName());
+            stampDataSet[index] = data;
         }
-        retStampMap.put("Stamps", stamps);
-        return retStampMap;
+        
+        return stampDataSet;
     }
     
     @Override
