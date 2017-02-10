c15fc4597aefcb081056882bc81f67c41bc74f40
Nolit <yaboonjapan@gmail.com>
2017-02-10 23:09:21 +0900

    Post処理が出来るよう変更

--- src/main/java/servlets/MyPageServlet.java
+++ src/main/java/servlets/MyPageServlet.java
@@ -40,11 +40,11 @@ public class MyPageServlet extends HttpServlet {
     UserManager um;
         
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("application/json;charset=UTF-8");
         
-        String email = request.getParameter("mailAddress");
+        String email = request.getParameter("email");
         String password = request.getParameter("password");
         Users user = um.readByEmailAndPassword(email, password);
         byte[] thumbnail = ImageUtil.read(user.getThumbnail());
